b1472fdb4ae2e7d058bf7c50fa540cbe207ea2b5
mozanunal@gmail.com <mehmetozanunal>
2018-02-17 09:14:58 +0300

    db.json empty. It is random init usong create api

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,12 @@ app.get('/', function (req, res) {
     res.send('hello world');
 });
 
+app.get('/courierRouter', function (req, res) {
+
+
+
+});
+
 
 /*
 Courier api
@@ -16,11 +22,61 @@ app.get('/api/courier', function (req, res) {
     res.send(parsedJSON.couriers);
 });
 
+app.get('/api/courier/:id', function (req, res) {
+    res.send(parsedJSON.couriers[req.params.id]);
+});
+
 /*
+Packet api
 */
 app.get('/api/packet', function (req, res) {
     res.send(parsedJSON.packets);
 });
 
+app.get('/api/packet/:id', function (req, res) {
+    res.send(parsedJSON.packets[req.params.id]);
+});
+
+
+app.get('/api/create', function (req, res) {
+    for (let i = 0; i < 3; i++) {
+        var courier = {
+            "_id": i,
+            "initLocation": {
+                "lat": 42.232,
+                "long": 23.32
+            },
+            "curLocation": {
+                "lat": 42.232,
+                "long": 23.32
+            },
+            "loadCapacity": 20,
+            "remainingCapacity": 20,
+            "route": {},
+            "packets": [
+            ]
+        };
+        parsedJSON.couriers.push(courier);
+    }
+
+    for (let i = 0; i < 5; i++) {
+        var packet = {
+            "_id": "dsadqwdwqdwqdqwd",
+            "initLocation": {
+                "lat": 42.232,
+                "long": 23.32
+            },
+            "destLocation": {
+                "lat": 42.232,
+                "long": 23.32
+            },
+            "weight": 20,
+            "state": 0,
+            "courier": {}
+        };
+        parsedJSON.packets.push(packet);
+    }
+    res.send(parsedJSON);
+});
 
 app.listen(3000);
