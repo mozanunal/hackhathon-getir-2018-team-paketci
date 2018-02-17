c25685f699cb96a7dad460b8ac8a82785af43d5f
mozanunal@gmail.com <mehmetozanunal>
2018-02-17 17:01:00 +0300

    cors enabled http header

--- backend/app.js
+++ backend/app.js
@@ -8,6 +8,13 @@ var pld = require('point-line-distance');
 function randomRange(min, max) {
     return ~~(Math.random() * (max - min + 1)) + min
 }
+
+app.all('/*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+  });
+
 /*
 Is it really working?
 */
